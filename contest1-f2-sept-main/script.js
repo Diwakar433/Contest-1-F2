5b95ec6e777f9d48c0b11d9e2d1dddc1cebbaef3
Diwakar Sharma <diwakarchiku12@gmail.com>
2023-12-16 22:02:10 +0530

    final

--- contest1-f2-sept-main/script.js
+++ contest1-f2-sept-main/script.js
@@ -4,31 +4,71 @@ const data = [
 ];
 
 // 1. Print Developers
-function printDeveloper() {}
+function printDeveloper() {
+  console.log('Developers:');
+  developersArray.forEach(developer => console.log(developer.name));
+}
 
 // 2. Add Data
-function addData() {}
+function addData() {
+  const name = prompt('Enter name:');
+  const age = prompt('Enter age:');
+  const profession = prompt('Enter profession:');
+  developersArray.push({ name, age: parseInt(age), profession });
+}
 
 // 3. Remove Admins
-function removeAdmin() {}
+function removeAdmin() {
+  developersArray = developersArray.filter(developer => developer.profession !== 'admin');
+}
 
 // 4. Concatenate Array
-function concatenateArray() {}
+function concatenateArray() {
+  const dummyArray = [
+    { name: 'Doe', age: 35, profession: 'manager' },
+    { name: 'Eve', age: 32, profession: 'developer' },
+  ];
+  const resultArray = developersArray.concat(dummyArray);
+  console.log('Concatenated Array:', resultArray);
+}
 
 // 5. Average Age
-function averageAge() {}
+function averageAge() {
+  const totalAge = developersArray.reduce((sum, developer) => sum + developer.age, 0);
+  const avgAge = totalAge / developersArray.length;
+  console.log('Average Age:', avgAge);
+}
 
 // 6. Age Check
-function checkAgeAbove25() {}
+function checkAgeAbove25() {
+  const isAbove25 = developersArray.some(developer => developer.age > 25);
+  console.log('Is there anyone above 25?', isAbove25);
+}
 
 // 7. Unique Professions
-function uniqueProfessions() {}
+function uniqueProfessions() {
+  const professions = [...new Set(developersArray.map(developer => developer.profession))];
+  console.log('Unique Professions:', professions);
+}
 
 // 8. Sort by Age
-function sortByAge() {}
+function sortByAge() {
+  developersArray.sort((a, b) => a.age - b.age);
+  console.log('Sorted by Age:', developersArray);
+}
 
 // 9. Update Profession
-function updateJohnsProfession() {}
+function updateJohnsProfession() {
+  const john = developersArray.find(developer => developer.name === 'John');
+  if (john) {
+    john.profession = 'manager';
+  }
+}
 
 // 10. Profession Count
-function getTotalProfessions() {}
+function getTotalProfessions() {
+  const developerCount = developersArray.filter(developer => developer.profession === 'developer').length;
+  const adminCount = developersArray.filter(developer => developer.profession === 'admin').length;
+  console.log('Total Developers:', developerCount);
+  console.log('Total Admins:', adminCount);
+}
